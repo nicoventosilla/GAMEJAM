e3e147be8d5a0e36866583b74c38bc4a5bcef9bc
Nico <86941298+nicoventosilla@users.noreply.github.com>
2024-10-26 22:12:19 -0500

    Segunda version
    funcionalidad basica

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
-#include <string>
 #include <cstdlib>
 #include <ctime>
 
@@ -18,20 +17,20 @@ void ocultarCursor() // Funcion para ocultar el cursor
 
 void gotoxy(int x, int y) // Funcion para posicionar el cursor en una coordenada
 {
-    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(consoleHandle, coord);
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el handle de la consola
+    COORD coord; // Estructura que contiene las coordenadas
+    coord.X = x; // Coordenada X
+    coord.Y = y; // Coordenada Y
+    SetConsoleCursorPosition(consoleHandle, coord); // Mover el cursor a la coordenada (x, y)
 }
 
 void color(int color) // Funcion para cambiar el color de la letra
 {
-    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(consoleHandle, color);
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); // Obtener el handle de la consola
+    SetConsoleTextAttribute(consoleHandle, color); // Cambiar el color de la letra
 }
 
-void dibujarCarro(int x, int y) // Funcion para dibujar un carro
+void Carro1(int x, int y) // Funcion para dibujar un carro
 {
     color(3);
     gotoxy(x, y);
@@ -42,6 +41,32 @@ void dibujarCarro(int x, int y) // Funcion para dibujar un carro
     cout << "(   _    _)";
     gotoxy(x, y + 3);
     cout << "=`(_)--(_)";
+}
+
+void Carro2(int x, int y) // Funcion para dibujar un carro
+{
+    color(4);
+    gotoxy(x, y);
+    cout << "  ______";
+    gotoxy(x, y + 1);
+    cout << " |   |  |_";
+    gotoxy(x, y + 2);
+    cout << " |___|__|_|";
+    gotoxy(x, y + 3);
+    cout << "  O    O";
+}
+
+void Carro3(int x, int y) // Funcion para dibujar un carro
+{
+    color(5);
+    gotoxy(x, y);
+    cout << "  ____";
+    gotoxy(x, y + 1);
+    cout << " /|_||\\___";
+    gotoxy(x, y + 2);
+    cout << "(   _    _|";
+    gotoxy(x, y + 3);
+    cout << " `-(_)-(_)";
 }
 
 void borrarCarro(int x, int y) // Funcion para borrar un carro
@@ -56,35 +81,96 @@ void borrarCarro(int x, int y) // Funcion para borrar un carro
     cout << "          ";
 }
 
-void pista()
+void pistaInferior()
+{
+    gotoxy(0, 30);
+    cout << "          ---------------------------------------------------------------------------------"; // 81 guiones
+    gotoxy(0, 31);
+    cout << "                                                                                          ";
+    gotoxy(0, 32);
+    cout << "    P                                                                                     ";
+    gotoxy(0, 33);
+    cout << "    A                                                                                     ";
+    gotoxy(0, 34);
+    cout << "    R                                                                                     ";
+    gotoxy(0, 35);
+    cout << "    T     ---------------------------------------------------------------------------------------------";
+    // 93 guiones
+    gotoxy(0, 36);
+    cout << "    I                                                                                     ";
+    gotoxy(0, 37);
+    cout << "    D                                                                                     ";
+    gotoxy(0, 38);
+    cout << "    A                                                                                     ";
+    gotoxy(0, 39);
+    cout << "                                                                                          ";
+    gotoxy(0, 40);
+    cout <<
+        "          ---------------------------------------------------------------------------------------------------------";
+    // 105 guiones
+}
+
+void pistaSuperior()
 {
     gotoxy(0, 0);
-    cout << "PARTIDA                                                                         ";
+    cout <<
+        "          ---------------------------------------------------------------------------------------------------------";
+    // 105 guiones
     gotoxy(0, 1);
-    cout << "                                                                                ";
+    cout << "                                                                                          ";
     gotoxy(0, 2);
-    cout << "                                                                                ";
+    cout << "    M                                                                                     ";
     gotoxy(0, 3);
-    cout << "                                                                                ";
+    cout << "                                                                                          ";
     gotoxy(0, 4);
-    cout << "                                                                                ";
+    cout << "    E                                                                                     ";
     gotoxy(0, 5);
-    cout << "--------------------------------------------------------------------------------";
+    cout << "          ---------------------------------------------------------------------------------------------";
+    // 93 guiones
     gotoxy(0, 6);
-    cout << "                                                                                ";
+    cout << "    T                                                                                     ";
     gotoxy(0, 7);
-    cout << "                                                                                ";
+    cout << "                                                                                          ";
     gotoxy(0, 8);
-    cout << "                                                                                ";
+    cout << "    A                                                                                     ";
     gotoxy(0, 9);
-    cout << "                                                                                ";
+    cout << "                                                                                          ";
     gotoxy(0, 10);
-    cout << "--------------------------------------------------------------------------------";
+    cout << "          ---------------------------------------------------------------------------------"; // 81 guiones
+}
+
+void pistaDerecha()
+{
+    for (int i = 29; i > 10; i--)
+    {
+        gotoxy(91, i);
+        cout << "|";
+    }
+
+    for (int i = 34; i > 5; i--)
+    {
+        gotoxy(103, i);
+        cout << "|";
+    }
+
+    for (int i = 39; i > 0; i--)
+    {
+        gotoxy(115, i);
+        cout << "|";
+    }
+}
+
+void pistas()
+{
+    pistaSuperior();
+    pistaInferior();
+    pistaDerecha();
 }
 
 void esperarTecla() // Funcion para esperar a que se presione una tecla
 {
-    cout << endl << "Presione <ESCAPE> para volver al menu principal" << endl;
+    gotoxy(25, 21);
+    cout << "Presione <ESCAPE> para volver al menu principal" << endl;
 
     while (true)
     {
@@ -123,43 +209,80 @@ void carrera()
 {
     system("cls");
 
+    pistas();
+
     srand(time(0));
 
-    pista();
+    int x1 = 10, y1 = 26;
+    int x2 = 10, y2 = 31;
+    int x3 = 10, y3 = 36;
 
-    dibujarCarro(0, 1);
-    dibujarCarro(0, 6);
+    Carro1(x1, y1);
+    Carro2(x2, y2);
+    Carro3(x3, y3);
 
-    int x1 = 0, x2 = 0;
-
-    while (x1 < 70 && x2 < 70)
+    while (true)
     {
-        borrarCarro(x1, 1);
-        borrarCarro(x2, 6);
+        borrarCarro(x1, y1);
+        borrarCarro(x2, y2);
+        borrarCarro(x3, y3);
 
-        x1 += rand() % 3;
-        x2 += rand() % 3;
+        // Movimiento del carro 1
+        if (x1 < 80 && y1 == 26)
+            x1++;
+        else if (x1 == 80 && y1 > 11)
+            y1--;
+        else if (y1 == 11 && x1 > 0)
+            x1--;
 
-        dibujarCarro(x1, 1);
-        dibujarCarro(x2, 6);
+        // Movimiento del carro 2
+        if (x2 < 92 && y2 == 31)
+            x2++;
+        else if (x2 == 92 && y2 > 6)
+            y2--;
+        else if (y2 == 6 && x2 > 0)
+            x2--;
+
+        // Movimiento del carro 3
+        if (x3 < 104 && y3 == 36)
+            x3++;
+        else if (x3 == 104 && y3 > 1)
+            y3--;
+        else if (y3 == 1 && x3 > 0)
+            x3--;
+
+        Carro1(x1, y1);
+        Carro2(x2, y2);
+        Carro3(x3, y3);
 
         Sleep(100);
-    }
 
-    if (x1 >= 70 && x2 >= 70)
+        // Condición de victoria
+        if ((x1 == 10 && y1 == 11) || (x2 == 10 && y2 == 6) || (x3 == 10 && y3 == 1))
+            break;
+    } // Fin del while
+
+    color(7); // Cambiar el color de la letra a blanco
+
+    if (x1 == 10 && y1 == 11 && x2 == 10 && y2 == 6 && x3 == 10 && y3 == 1)
     {
-        gotoxy(0, 12);
+        gotoxy(40, 20);
         cout << "Empate!";
     }
-    else if (x1 >= 70)
+    else if (x1 == 10 && y1 == 11)
     {
-        gotoxy(0, 12);
+        gotoxy(40, 20);
         cout << "Gana el carro 1!";
     }
-    else
+    else if (x2 == 10 && y2 == 6)
     {
-        gotoxy(0, 12);
+        gotoxy(40, 20);
         cout << "Gana el carro 2!";
+    }
+    else if (x3 == 10 && y3 == 1)
+    {
+        gotoxy(40, 20);
+        cout << "Gana el carro 3!";
     }
 
     esperarTecla();
@@ -191,15 +314,16 @@ int main()
         switch (tecla)
         {
         case '1':
-            carrera();
+            carrera(); // Inicia la carrera
             break;
         case '2':
-            creditos();
+            creditos(); // Muestra los creditos
             break;
         case '3':
             salir();
             return 0; /// Termina el programa
         }
     }
+
     return 0;
 }
